0971382772a0e543a264ced2123af0d1b164f7d3
marcelosousa <dipython@gmail.com>
2015-03-31 02:43:35 +0100

    pthread function calls

--- src/Frontend/Frontend.hs
+++ src/Frontend/Frontend.hs
@@ -32,11 +32,15 @@ pass1 p@(Program (decl,defs)) =
         (threadCount, threads, main') = findAndInstrThreads main
         pmt = GlobalDecl 0 (Index (Ident "__poet_mutex_threads") (Const $ IntValue $ toInteger threadCount)) Nothing
         pmd = GlobalDecl 0 (Ident "__poet_mutex_death") (Just (IntValue 0))
-    in (Program (pmt:pmd:decl,replaceMain main' defs),(threadCount,threads))
+        threadNames = fst $ unzip threads
+        defs' = replaceMain main' defs
+        -- introduce a call to pthread_exit
+        defs'' = map (introducePthreadCalls threadNames) defs'
+        -- replace pthread_exit with mutex_unlock(__poet_mutex_threads[i]) + mutex_lock(__poet_mutex_death)
+        defs''' = map (replacePthreadExit ("main":threadNames)) defs''
+    in (Program (pmt:pmd:decl,defs'''),(threadCount,threads))
 
--- introduce a call to pthread_exit as the last statement of every thread and main
--- replace pthread_exit with mutex_unlock(__poet_mutex_threads[i]) + mutex_lock(__poet_mutex_death)
--- at the beginning of every thread, do a mutex_lock(__poet_mutex_threads[i])
+-- replace 
 
 -- pass 3: rename variable x to "c.x"
 
@@ -51,7 +55,40 @@ replaceMain :: Definition -> [Definition] -> [Definition]
 replaceMain nmain defs = 
     let others = filter (not . isMain) defs
     in nmain:others
-    
+
+introducePthreadCalls :: [Ident] -> Definition -> Definition
+introducePthreadCalls names f@(FunctionDef pc "main" params stats) = 
+    let mcall = ExprStat pc $ Call "pthread_exit" [Ident "NULL"]
+    in FunctionDef pc "main" params $ stats++[mcall]
+introducePthreadCalls names f@(FunctionDef pc name params stats) =     
+    let mcall = ExprStat pc $ Call "pthread_exit" [Ident "NULL"]
+        pcall = ExprStat pc (Call "__poet_mutex_lock" [Index (Ident "__poet_mutex_threads") (Ident name)])
+    in if name `elem` names 
+       then FunctionDef pc name params $ pcall:stats++[mcall]
+       else f
+
+replacePthreadExit :: [Ident] -> Definition -> Definition
+replacePthreadExit names f@(FunctionDef pc name params stats) = 
+    if name `elem` names
+    then let stats' = foldr (\s r -> replacePthreadExitAux name s ++ r) [] stats
+         in FunctionDef pc name params stats'
+    else f
+
+replacePthreadExitAux :: Ident -> AnnStatement PC -> [AnnStatement PC]
+replacePthreadExitAux "main" s =
+    case s of
+         ExprStat pc (Call "pthread_exit" [Ident "NULL"]) ->
+           let dcall = ExprStat pc (Call "__poet_mutex_lock" [Ident "__poet_mutex_death"])
+           in [dcall]
+         _ -> [s]
+replacePthreadExitAux name s =
+    case s of
+         ExprStat pc (Call "pthread_exit" [Ident "NULL"]) ->
+           let pcall = ExprStat pc (Call "__poet_mutex_lock" [Index (Ident "__poet_mutex_threads") (Ident name)])
+               dcall = ExprStat pc (Call "__poet_mutex_lock" [Ident "__poet_mutex_death"])
+           in [pcall,dcall]
+         _ -> [s]
+
 findAndInstrThreads :: Definition -> (Int,[(Ident,Int)], Definition)
 findAndInstrThreads (FunctionDef pc "main" params stats) = 
     let (threadCount, threads, stats') = foldr (\s (i,r,r') -> let (s',t,i') = findAndInstrThreadsAux s i in (i',t++r,s'++r')) (0,[],[]) stats
@@ -68,15 +105,15 @@ findAndInstrThreadsAux s threadCount =
               ass = ExprStat pc (Assign CAssignOp (Ident tid) i)
               mcall = ExprStat pc (Call "__poet_mutex_lock" [Index (Ident "__poet_mutex_threads") i])
           in ([ass,mcall], [(name ,threadCount)], threadCount+1)
-        ExprStat pc (Call "pthread_join" [Ident tid, Ident "NULL"]) -> 
-          let mcall = ExprStat pc (Call "__poet_mutex_lock" [Index (Ident "__poet_mutex_threads") (Ident tid)])
+        ExprStat pc (Call "pthread_join" [Ident name, Ident "NULL"]) -> 
+          let mcall = ExprStat pc (Call "__poet_mutex_lock" [Index (Ident "__poet_mutex_threads") (Ident name)])
           in ([mcall], [], threadCount)
         ExprStat _ (Call fname _) ->
           if fname `elem` ["pthread_create", "pthread_join"]
           then error "unexpected parameters for pthread_{create,join}"
           else ([s],[],threadCount) 
         _ -> ([s],[],threadCount)                     
-        
+
 simplify :: Program -> Program
 simplify (Program (decl,defs)) = 
     let defs' = fst $ unzip $ map (applyTrans iDB for2while) defs -- Pass 4
