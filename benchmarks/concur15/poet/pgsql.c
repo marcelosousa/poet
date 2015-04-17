c3b855ee42166c1af0c600f2b1e5192a2d7c2ed9
Subodh Sharma <subodh.v.sharma@gmail.com>
2015-04-17 17:54:50 +0530

    pgsql.c fixed

--- benchmarks/concur15/poet/pgsql.c
+++ benchmarks/concur15/poet/pgsql.c
@@ -2,67 +2,76 @@
 
 /* BOUND 8 */
 
-#define FENCE() asm volatile ("mfence" ::: "memory")
+/* #define FENCE() asm volatile ("mfence" ::: "memory") */
 
-#ifdef ENABLE_PSO_FENCES
-#define PSO_FENCE() FENCE()
-#else
-#define PSO_FENCE() /*No FENCE*/
-#endif
+/* #ifdef ENABLE_PSO_FENCES */
+/* #define PSO_FENCE() FENCE() */
+/* #else */
+/* #define PSO_FENCE() /\*No FENCE*\/ */
+/* #endif */
 
-#include <stdbool.h>
-#include <assert.h>
-#include <pthread.h>
+//#include <stdbool.h>
+//#include <assert.h>
+#include "pthread.h"
 
-void __VERIFIER_assume(int);
+//void __VERIFIER_assume(int);
 
-volatile _Bool latch1 = true;
-volatile _Bool flag1  = true;
-volatile _Bool latch2 = false;
-volatile _Bool flag2  = false;
+volatile int latch1 = 1;
+volatile int flag1  = 1;
+volatile int latch2 = 0;
+volatile int flag2  = 0;
 
-_Bool __unbuffered_tmp2 = false;
+int __unbuffered_tmp2 = 0;
 
-void* worker_1(void* arg)
+void* worker_1()
 {
   for(;;) {
-    __VERIFIER_assume(latch1);
-    assert(!latch1 || flag1);
-    latch1 = false;
+    // __VERIFIER_assume(latch1);
+  L1: if(latch1 != 1) goto L1;
+    //assert(!latch1 || flag1);
+    if (latch1) __poet_fail();
+    if(!flag1) __poet_fail();
+
+    latch1 = 0;
     if(flag1) {
-      flag1 = false;
-      flag2 = true;
-      PSO_FENCE();
-      latch2 = true;
+      flag1 = 0;
+      flag2 = 1;
+      //PSO_FENCE();
+      latch2 = 1;
     }
   }
-  return NULL;
+  //return NULL;
 }
 
-void* worker_2(void* arg)
+void* worker_2()
 {
   for(;;) {
-    __VERIFIER_assume(latch2);
-    assert(!latch2 || flag2);
-    latch2 = false;
+    //    __VERIFIER_assume(latch2);
+  L2: if(!latch2) goto L2;
+    
+    //    assert(!latch2 || flag2);
+    if(latch2) __poet_fail();
+    if(!flag2) __poet_fail();
+    latch2 = 0;
     if(flag2) {
-      flag2 = false;
-      flag1 = true;
-      PSO_FENCE();
-      latch1 = true;
+      flag2 = 0;
+      flag1 = 1;
+      //      PSO_FENCE();
+      latch1 = 1;
     }
   }
-  return NULL;
+  //return NULL;
 }
 
 int main() {
-  #ifndef GOTO
-  pthread_t t1, t2;
-  pthread_create(&t1, 0, worker_1, NULL);
-  pthread_create(&t2, 0, worker_2, NULL);
-  #else
-  __CPROVER_ASYNC_0: worker_1(NULL);
-  worker_2(NULL);
-  #endif
-  return 0;
+  //  #ifndef GOTO
+  pthread_t t1;
+  pthread_t t2;
+  pthread_create(t1, NULL, worker_1, NULL);
+  pthread_create(t2, NULL, worker_2, NULL);
+  //  #else
+    // __CPROVER_ASYNC_0: worker_1(NULL);
+  //worker_2(NULL);
+  // #endif
+  //return 0;
 }
