32276a31e26602fbc8299f8cbde180575b4d9189
Cesar Rodriguez <cesar.rodriguez.ortega@gmail.com>
2015-10-14 08:58:07 +0000

    Better names :)

--- benchmarks/regression/intervals/nondet_gt-safe.c
+++ benchmarks/regression/intervals/nondet_gt-safe.c
@@ -2,5 +2,5 @@
 int main (void)
 {
 	int x = nondet (1, 10);
-	if (x >= 15) __poet_fail ();
+	if (x > 15) __poet_fail ();
 }
--- benchmarks/regression/intervals/nondet_ne-safe.c
+++ benchmarks/regression/intervals/nondet_ne-safe.c
@@ -2,5 +2,5 @@
 int main (void)
 {
 	int x = 5;
-	if (x == 5) __poet_fail ();
+	if (x != 5) __poet_fail ();
 }
