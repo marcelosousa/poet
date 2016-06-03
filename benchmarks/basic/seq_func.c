458607db95a9a99a9e4ad62f66d7f22d88d425a2
marcelosousa <dipython@gmail.com>
2016-06-03 19:33:51 -0400

    working on complete version

--- benchmarks/basic/seq_func.c
+++ benchmarks/basic/seq_func.c
@@ -3,13 +3,17 @@ int test (int x) {
   return x+1;
 }
 */
+/*
 int main_test () {
   int i = 0;
   //i = test(i);
   return i;
 }
+*/
+int *m;
+int main_test(int j);
 
-int bla () {
+int bla (int k) {
   int i = 0;
   return i;
 }
