8b65582ae0e4d2dd1e1e5eeef1ce45e38008e8a1
marcelosousa <dipython@gmail.com>
2016-06-20 19:30:23 -0400

    working on new api

--- src/Model/GCS.hs
+++ src/Model/GCS.hs
@@ -1,4 +1,5 @@
 {-#LANGUAGE RecordWildCards, TypeSynonymInstances, FlexibleInstances, DoAndIfThenElse #-}
+{-#LANGUAGE MultiParamTypeClasses #-}
 module Model.GCS where
 
 -- Data Structures
@@ -12,27 +13,59 @@ import qualified Data.Vector as V
 import qualified Data.Word as W
 import Debug.Trace
 --import Domain.Concrete
-import Language.SimpleC.AST (PC, Statement)
+import Language.SimpleC.AST 
+import Language.SimpleC.Flow
+import Language.SimpleC.Converter
 import Util.Generic
 
--- System is a vector of transitions and an initial state
--- We use vector because we just want to query. 
-data System st = 
-  System {
-    transitions :: Transitions st,
-    initialState :: st,
-    initialActs :: Acts
+-- A System is a collection of CFGs together with
+-- a representation of the frontier on the PCs 
+-- which denotes the number of enabled threads
+-- and their position in the CFG. 
+-- The CFGs are annotated with states at each node,
+-- as in usual abstract interpretation where these
+-- states are elements of a lattice.
+-- These states not only contain valuations but
+-- also information regarding the actions to
+-- compute an independence relation on the fly.
+-- AbsSystem denotes an abstract system that 
+-- receives as type parameters: a representation 
+-- for the state and a represetation for the actions.
+type Frontier = [NodeId]
+data System st act = 
+  System 
+  { frnt :: Frontier                 -- ^ Frontier in the CFGs
+  , gbst :: st                       -- ^ Global State
+  , gbac :: [act]                    -- ^ Actions 
+  , cfgs :: Graphs SymId () (st,act) -- ^ Control Flow Graphs
+  , symt :: Map SymId Symbol         -- ^ Symbol Table
+  , thds :: [ThreadId]               -- ^ Thread ids
+  , tcnt :: Int                      -- ^ Counter for th id
   }
+  deriving Show
 
-type ProcessID = BS.ByteString
-type TransitionID = Int 
-type TransitionsID = V.Vector TransitionID
-type Transitions st = V.Vector (Transition st)
-type TransitionInfo = (ProcessID, TransitionID, Statement, Acts)
-type Transition st = (TransitionInfo, TransitionFn st)
-type TransitionFn st = st -> [st]
+-- Thread ID
+type ThreadId = Int
 
+-- | botID -1 is the transition id for bottom 
+botID :: ThreadId 
+botID = -1
 
+class Action act where
+  varOf :: act -> Variable
+  isBlocking :: [act] -> Bool
+
+class Projection st where
+  controlPart :: st -> st
+  dataPart :: st -> st
+  subsumes :: st -> st -> Bool
+
+class (Action act, Projection st) => Collapsible st act where
+  enabledThreads :: System st act -> st -> [ThreadId]
+  collapse :: System st act -> st -> ThreadId -> [(st,[act])]
+
+-- Default implementation of an
+-- Action.
 data Variable = V Var | A Var Integer
   deriving (Show,Eq,Ord)
   
@@ -40,50 +73,10 @@ type Acts = [Act]
 data Act = Lock Variable | Unlock Variable | Other
   deriving (Show,Eq,Ord)
 
-varOf :: Act -> Variable
-varOf (Lock v) = v
-varOf (Unlock v) = v
-varOf Other = error "varOf Other"
-
-isBlocking :: Acts -> Bool
-isBlocking = any (\act -> act /= Other)
-
-class Projection st where
-  controlPart :: st -> st
-  dataPart :: st -> st
-  subsumes :: st -> st -> Bool
-  
--- | enabledTransitions
-enabledTransitions :: System st -> st -> V.Vector TransitionInfo
-enabledTransitions sys@System{..} s =
-  let enTr = V.filter (\(_,t) -> not $ null $ t s) transitions
-  in V.map fst enTr
-
--- | botID 0 is the transition id for bottom 
-botID :: TransitionID
-botID = -1
-
--- | bottom transition is simply: return . id
-bot :: TransitionFn st
-bot = (:[])
-
--- GETTERS
-
--- | getTransition - 
-getTransition :: System st -> TransitionID -> TransitionFn st
-getTransition sys@System{..} trIdx
-  | trIdx == botID = bot
-  | otherwise = 
-      case transitions V.!? trIdx of
-        Nothing -> error $ "getTransition fail: " ++ show trIdx
-        Just (_,tr) -> tr
-
-getTransitionWithID :: System st -> TransitionID -> TransitionFn st
-getTransitionWithID sys@System{..} trID = 
-  case transitions V.!? trID of
-    Nothing -> error $ "getTransition fail: " ++ show trID
-    Just ((_,trIDx,_,_),tr) -> 
-      if trID /= trIDx
-      then error $ "getTransitionWithID something went wrong before: " ++ show (trID, trIDx)
-      else tr
-
+instance Action Act where
+  -- varOf :: Act -> Variable
+  varOf (Lock v) = v
+  varOf (Unlock v) = v
+  varOf Other = error "varOf Other"
+  -- isBlocking :: Acts -> Bool
+  isBlocking = any (\act -> act /= Other)
