931ce88eae77056225adce98f6fcf8e0da590214
marcelosousa <dipython@gmail.com>
2016-07-03 22:20:39 -0400

    working on it

--- src/Domain/Interval/State.hs
+++ src/Domain/Interval/State.hs
@@ -22,6 +22,8 @@ import Language.SimpleC.AST
 import Language.SimpleC.Util
 
 import Domain.Util
+import Domain.Interval.Value
+
 
 -- | Interval Memory Cell
 type IntMCell = MemCell SymId () IntValue
@@ -49,7 +51,7 @@ data ThState =
   ThState
   { 
     pos :: Pos
-  , id :: Tid
+  , id :: TId
   , locals :: Locals 
   } 
   deriving (Show,Eq,Ord)
@@ -59,7 +61,7 @@ empty_state :: IntState
 empty_state = IntState M.empty M.empty 0 False 
 
 -- | Set the position in the cfg of a thread
-set_pos :: IntState -> TId -> Pos -> Sigma
+set_pos :: IntState -> TId -> Pos -> IntState 
 set_pos st@IntState{..} tid npos = 
   let th_st' =
         case M.lookup tid th_states of
@@ -76,8 +78,8 @@ set_pos st@IntState{..} tid npos =
 --    is greater or equal
 -- 3. Check the heap
 -- 4. Check the thread states
-subsumes_concrete :: Sigma -> Sigma -> Bool
-subsumes_concrete st1 st2 =
+subsumes_interval :: IntState -> IntState -> Bool
+subsumes_interval st1 st2 =
   case check_bottoms (is_bot st1) (is_bot st2) of
     Just r -> r
     Nothing ->
@@ -104,7 +106,7 @@ subsumes_concrete st1 st2 =
          in if pos th1 == pos th2
             then M.foldrWithKey' (\sym vals b -> check_locals sym vals lcs1 && b) True (locals th2)  
             else False
-   check_locals :: SymId -> ConValue -> Map SymId ConValue -> Bool 
+   check_locals :: SymId -> IntValue -> Map SymId IntValue -> Bool 
    check_locals sym val2 lcs1 =
      case M.lookup sym lcs1 of
        Nothing -> False
@@ -124,36 +126,34 @@ instance Projection IntState where
   isBottom = is_bot 
 
 -- | API for modifying the state
-
-{-
 -- | insert_heap: inserts an element to the heap
-insert_heap :: Sigma -> SymId -> ConMCell -> Sigma
-insert_heap st@Sigma{..} id cell =
+insert_heap :: IntState -> SymId -> IntMCell -> IntState
+insert_heap st@IntState{..} id cell =
   let heap' = M.insert id cell heap
   in st {heap = heap'}  
 
-modify_heap :: Sigma -> SymId -> ConValue -> Sigma
-modify_heap st@Sigma{..} id val = 
+modify_heap :: IntState -> SymId -> IntValue -> IntState
+modify_heap st@IntState{..} id val = 
   let heap' = M.update (update_conmcell val) id heap
   in st {heap = heap'}
 
-update_conmcell :: ConValue -> ConMCell -> Maybe ConMCell
+update_conmcell :: IntValue -> IntMCell -> Maybe IntMCell
 update_conmcell nval c@MCell{..} = Just $ c { val = nval } 
 
 -- | insert_local: inserts an element to local state 
-insert_local :: Sigma -> TId -> SymId -> ConValue -> Sigma
+insert_local :: IntState -> TId -> SymId -> IntValue -> IntState
 insert_local = error "insert_local" 
 
 -- | modify the state: receives a MemAddrs and a
---   ConValue and assigns the ConValue to the MemAddrs
-modify_state :: Scope -> Sigma -> MemAddrs -> ConValue -> Sigma
+--   IntValue and assigns the IntValue to the MemAddrs
+modify_state :: Scope -> IntState -> MemAddrs -> IntValue -> IntState
 modify_state scope st addrs vals =
   case addrs of
     MemAddrTop -> error "modify_state: top addrs, need to traverse everything"
     MemAddrs l -> foldr (\a s -> modify_state' scope s a vals) st l
  where
-   modify_state' :: Scope -> Sigma -> MemAddr -> ConValue -> Sigma
-   modify_state' scope st@Sigma{..} add@MemAddr{..} conval =
+   modify_state' :: Scope -> IntState -> MemAddr -> IntValue -> IntState
+   modify_state' scope st@IntState{..} add@MemAddr{..} conval =
      -- First search in the heap 
      case M.lookup base heap of
        Nothing ->
@@ -162,9 +162,8 @@ modify_state scope st addrs vals =
            Global -> error "modify_state: id is not the heap and scope is global"
            Local i -> insert_local st i base conval 
        Just _ -> modify_heap st base conval  
--} 
-{-
-instance Hashable Sigma where
+
+instance Hashable IntState where
   hash = hash . M.toList
   hashWithSalt s st = hashWithSalt s $ M.toList st
   
@@ -176,8 +175,9 @@ instance Hashable Value where
     IntVal i -> hashWithSalt s i
     Array vals -> hashWithSalt s vals
 
+{-
 -- State equality: because I'm using a hashtable I need to stay within the ST monad
-isEqual :: Sigma s -> Sigma s -> ST s Bool
+isEqual :: IntState s -> IntState s -> ST s Bool
 isEqual s1 s2 = do
   l1 <- H.toList s1
   l2 <- H.toList s2
