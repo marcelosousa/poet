e82d28056447720064aa6163d814d3c23063a38d
marcelosousa <dipython@gmail.com>
2017-02-03 04:48:51 -0500

    removed data races

--- benchmarks/basic/ssbexp.c
+++ benchmarks/basic/ssbexp.c
@@ -1,8 +1,10 @@
 #include <pthread.h>
 #include <assert.h>
 
-#define K 6
+#define K 8
 
+pthread_mutex_t ma[K];
+pthread_mutex_t mi;
 int a[K];
 int i = 0;
 
@@ -10,30 +12,44 @@ void *counter()
 {
  for (int x = 1; x < K; x++)
  {
-   i = x; 
+   pthread_mutex_lock(&mi);
+     i = x; 
+   pthread_mutex_unlock(&mi);
  }
 }
 
 void *wa(void *aux)
 {
  int x = *((int *) aux);
- a[x] = x;
+ pthread_mutex_lock(&ma[x]);
+   a[x] = x;
+ pthread_mutex_unlock(&ma[x]);
 }
 
 int main()
 {
   pthread_t idk;
   pthread_t idw[K];
+  pthread_mutex_init(&mi, NULL);
 
-  pthread_create(&idk, NULL, counter, NULL);
   for (int x = 0; x < K; x++)
   {
     int *arg = malloc(sizeof(*arg));
     *arg = x;
+    pthread_mutex_init(&ma[x], NULL);
     pthread_create(&idw[x], 0, wa, arg);
   }
 
-  assert (a[i] >= 0);
+  pthread_create(&idk, NULL, counter, NULL);
+
+  int idx = 0;
+  pthread_mutex_lock (&mi);
+    idx = i;
+  pthread_mutex_unlock (&mi);
+
+  pthread_mutex_lock (&ma[idx]);
+    assert (a[idx] >= 0);
+  pthread_mutex_unlock (&ma[idx]);
 
   for (int x = 0; x < K; x++)
     pthread_join(idw[x],NULL);
